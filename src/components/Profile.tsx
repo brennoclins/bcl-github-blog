c3c5f5ce824329551e24344718ce37f4fa158f33
Brenno - BCL-ST <dev@bcl-st.com.br>
2024-04-03 10:45:58 -0300

    feat: fetching data from a user with the github api

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,32 +1,94 @@
-import { ArrowSquareOut, Buildings, GithubLogo, Users } from "phosphor-react"
-import styles from "./profile.module.css"
+import { ArrowSquareOut, Buildings, GithubLogo, Users } from "phosphor-react";
+import styles from "./profile.module.css";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+
+type User = {
+  id: number;
+  login: string;
+  name: string;
+  bio: string | null;
+  company: string | null;
+  avatar_url: string;
+  email: string | null;
+  followers: number;
+  following: number;
+  html_url: string;
+};
 
 export function Profile() {
+  const [user, setUser] = useState<User | null>(null);
+
+  async function getUser(username: string) {
+    const response = await api.get<User>(`users/${username}`);
+    const {
+      id,
+      login,
+      name,
+      bio,
+      company,
+      avatar_url,
+      email,
+      followers,
+      following,
+      html_url,
+    } = response.data;
+
+    setUser({
+      id,
+      login,
+      name,
+      bio,
+      company,
+      avatar_url,
+      email,
+      followers,
+      following,
+      html_url,
+    });
+  }
+
+  useEffect(() => {
+    getUser("zenorocha");
+  }, []);
+
   return (
     <section className={styles.profile}>
       <div className={styles.profileAvatar}>
-        <img src="https://github.com/brennoclins.png" alt="" />
+        <img src={user?.avatar_url} alt="" />
       </div>
 
       <div className={styles.profileContent}>
         <section className={styles.profileTitle}>
-          <h1>Brenno C. Lins</h1>
-          <a href="https://github.com/brennoclins" target="_blank" rel="noopener noreferrer">
+          <h1>{user?.name}</h1>
+          <a
+            href={user?.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             GITHUB
             <ArrowSquareOut size={18} />
           </a>
         </section>
 
         <section className={styles.profileDescription}>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis aperiam illo aliquid, sed iusto, neque dolor rerum dignissimos excepturi repellendus officia incidunt eius minima praesentium ipsam quos esse velit aspernatur.</p>
+          <p>
+            {user?.bio}
+          </p>
         </section>
 
         <section className={styles.profileTags}>
-          <span><GithubLogo size={26}/> brennoclins</span>
-          <span><Buildings size={26} /> BCL-ST</span>
-          <span><Users size={26} /> 53 seguidores</span>
+          <span>
+            <GithubLogo size={26} /> {user?.login}
+          </span>
+          <span>
+            <Buildings size={26} /> {user?.company}
+          </span>
+          <span>
+            <Users size={26} /> {user?.followers} seguidores
+          </span>
         </section>
       </div>
     </section>
-  )
+  );
 }
